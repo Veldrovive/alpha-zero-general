c71209dbfb6a652e853f2adda725b4ecf492494c
Veldrovive <aidan.dempster@gmail.com>
2021-03-07 16:20:07 -0500

    Added in the possibility for a draw

--- ProjectArena.py
+++ ProjectArena.py
@@ -27,6 +27,11 @@ def play_gomoku_auto(player_1: Player, player_2: Player, r_seed=None, verbose=Fa
     p1_times = []
     p2_times = []
 
+    def get_winner(game_res):
+        if abs(game_res) < 1:
+            return None
+        return game_res
+
     while True:
         s = time()
         move_y, move_x = player_1.move(board, 1, tournament_style=False)
@@ -39,9 +44,12 @@ def play_gomoku_auto(player_1: Player, player_2: Player, r_seed=None, verbose=Fa
             print_board(game, board)
         game_res = game.getGameEnded(board, 1)
         if game_res != 0:
+            winner = get_winner(game_res)
             if verbose:
-                print("White WON!!")
-            return 1, p1_times, p2_times
+                if winner is None:
+                    print("Draw")
+                print("White WON!!" if game_res == 1 else "Black WON!!")
+            return winner, p1_times, p2_times
 
         s = time()
         move_y, move_x = player_2.move(board, -1, tournament_style=False)
@@ -54,9 +62,12 @@ def play_gomoku_auto(player_1: Player, player_2: Player, r_seed=None, verbose=Fa
             print_board(game, board)
         game_res = game.getGameEnded(board, -1)
         if game_res != 0:
+            winner = get_winner(game_res)
             if verbose:
-                print("Black WON!!")
-            return -1, p1_times, p2_times
+                if winner is None:
+                    print("Draw")
+                print("White WON!!" if game_res == 1 else "Black WON!!")
+            return winner, p1_times, p2_times
 
 def get_stats(times, verbose=False):
     avg, std = np.average(times), np.std(times)
@@ -68,7 +79,7 @@ def get_stats(times, verbose=False):
 if __name__ == "__main__":
     player_1 = Player(50)
     player_2 = Player("checkpoints/checkpoint_20.pth.tar", 50)
-    _, p1_times, p2_times = play_gomoku_auto(player_1, player_2, r_seed=None, verbose=False)
+    _, p1_times, p2_times = play_gomoku_auto(player_1, player_2, r_seed=None, verbose=True)
     print("Player 1:")
     get_stats(p1_times, True)
     print("Player 2:")
