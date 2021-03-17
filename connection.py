6caeb7537d43f22e174974d32e2dd046acd26cdc
Veldrovive <aidan.dempster@gmail.com>
2021-03-16 21:42:09 -0400

    Changed it so that all old train examples are thrown out

--- connection.py
+++ connection.py
@@ -10,10 +10,51 @@ import numpy as np
 inverse_content = {
     "b": -1,
     "-": 0,
-    "1": 1
+    "w": 1
 }
 
 game = GomakuGame(8)
+
+class CompetitionPlayer:
+    def __init__(self, net_args: dotdict, checkpoint="./checkpoints/best.pth.tar", iterations: int=50):
+        self.checkpoint = checkpoint
+        self.size = game.getBoardSize()[0]
+        self.mcts_args = dotdict({'numMCTSSims': iterations, 'cpuct': 1.0})
+        self.net_args = net_args
+        self.net = NNet(game, self.net_args)
+        self.mcts = MCTS(game, self.net, self.mcts_args)
+
+    def from_string_array(self, board_seed):
+        board = np.zeros((self.size, self.size))
+        for y in range(self.size):
+            for x in range(self.size):
+                val = inverse_content[board_seed[y][x]]
+                board[y, x] = val
+        return board
+
+    def action_to_yx(self, action):
+        action_y, action_x = action // self.size, action % self.size
+        return action_y, action_x
+
+    def yx_to_action(self, y, x):
+        return y * self.size + x
+
+    def move(self, board, player):
+        player = 1 if player == "w" else -1
+        board = self.from_string_array(board)
+
+        canonical_board = game.getCanonicalForm(board, player)
+        action_probs = self.mcts.getActionProb(canonical_board, temp=0)
+        action = np.argmax(action_probs)
+        valid_actions = game.getValidMoves(canonical_board, 1)
+
+        if valid_actions[action] == 0:
+            # Then the agent tried to play an invalid move.
+            # Mask the probs with the valid actions and select from that.
+            valid_action_probs = action_probs*valid_actions
+            action = np.argmax(valid_action_probs)
+
+        return self.action_to_yx(action)
 
 
 class Player:
